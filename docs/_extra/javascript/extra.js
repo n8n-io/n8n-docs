97b449db74e677cecda304f7f8e4a05c1e0857e5
Deborah Barnard <deborah@starfallprojects.co.uk>
2024-06-24 12:26:34 +0100

    add passive

--- docs/_extra/javascript/extra.js
+++ docs/_extra/javascript/extra.js
@@ -1,7 +1,9 @@
 
 var n8nTopNav = document.getElementById("n8n-top-nav-target");
 // Hide the n8n additional top nav on scroll
-window.addEventListener('scroll', function() {
+window.addEventListener(
+  'scroll', 
+  function() {
 	if(scrollY != 0) {
 		n8nTopNav.classList.remove("n8n-visible");
 		n8nTopNav.classList.add("n8n-hidden");
@@ -9,4 +11,8 @@ window.addEventListener('scroll', function() {
 		n8nTopNav.classList.remove("n8n-hidden");
 		n8nTopNav.classList.add("n8n-visible");
 	}
-  });
+  },
+  {
+	passive: true
+  }
+);

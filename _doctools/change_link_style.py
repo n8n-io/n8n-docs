ef7472148510da944670f2bcd9b121c8a4b9aa47
Justin Ellingwood <justin@n8n.io>
2025-02-10 10:04:30 +0000

    Clean up out-of-date comments from script

--- _doctools/change_link_style.py
+++ _doctools/change_link_style.py
@@ -11,7 +11,7 @@ def map_urls_to_files(path_list, root_dir):
 	"""Return a dictionary mapping URL link targets to file targets."""
 	path_maps = dict()
 	for path in path_list:
-		# Here, we make convert the file path to absolute from the root_dir
+		# Here, we convert file paths to absolute paths from the root_dir
 		md_link_target = "/" / path.relative_to(root_dir)
 		url_link_target = get_url_path_from_file_path(md_link_target)
 		path_maps[url_link_target] = str(md_link_target)
@@ -19,14 +19,17 @@ def map_urls_to_files(path_list, root_dir):
 	return path_maps
 
 def get_url_path_from_file_path(file_path):
-	"""Return the rendered URL path from a given file path."""
+	"""Return the URL path for a given file path."""
 	if file_path.name == "index.md":
 		return str(file_path.parent)
 	else:
 		return str(file_path.parent / file_path.stem)
 
 def main(directory):
-	"""Finds Markdown files, extracts 'contentType' and word count, then prints and saves results."""
+	"""
+	Finds Markdown files, calculates URL paths associated with them, then finds and
+	replaces URL paths with file paths in links throughout the repo.
+	"""
 	markdown_files = find_markdown_files(directory)
 	snippet_files = find_markdown_files("../_snippets")
 	link_maps = map_urls_to_files(markdown_files, directory)
@@ -35,11 +38,9 @@ def main(directory):
 		text = file.read_text()
 
 		for url_target, file_target in link_maps.items():
-			# This isn't adequate. Need to account for ID components
 			find_pattern = fr"]\({url_target}/?(#[0-9A-Za-z-]+)?\)"
 			replace_pattern = fr"]({file_target}\1)"
 			text = re.sub(find_pattern, replace_pattern, text)
-			#text = text.replace(f"]({url_target})", f"]({file_target})")
 
 		file.write_text(text)
 

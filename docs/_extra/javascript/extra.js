7e9323633010340c9cce276abed7f86c269c84d3
David Karlsson <35727626+dvdksn@users.noreply.github.com>
2025-05-15 16:25:52 +0200

    feat: add kapa.ai widget
    Signed-off-by: David Karlsson <35727626+dvdksn@users.noreply.github.com>

---
+++ docs/_extra/javascript/extra.js
@@ -0,0 +1,16 @@
+document.addEventListener("DOMContentLoaded", function () {
+    var script = document.createElement('script');
+    script.src = 'https://widget.kapa.ai/kapa-widget.bundle.js';
+    script.setAttribute('data-website-id', 'd2598b63-bfa7-4ddd-a0ac-e6c69f4d0653');
+    script.setAttribute('data-project-name', 'n8n');
+    script.setAttribute('data-project-color', '#EA4B71');
+    script.setAttribute('data-project-logo', 'https://avatars.githubusercontent.com/u/45487711?s=280&v=4');
+    script.setAttribute('data-modal-disclaimer', 'This is a custom LLM for n8n with access to developer docs and the [n8n community forum](https://community.n8n.io/).');
+    script.setAttribute('data-font-size-xs', '0.65rem');
+    script.setAttribute('data-font-size-sm', '0.75rem');
+    script.setAttribute('data-font-size-md', '0.85rem');
+    script.setAttribute('data-font-size-lg', '0.95rem');
+    script.setAttribute('data-font-size-xl', '1.05rem');
+    script.setAttribute('data-modal-size', '800px');
+    document.body.appendChild(script);
+});

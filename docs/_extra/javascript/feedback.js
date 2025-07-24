1043814cd1bec031423c1d2640209dec55829ea3
unknown <deborah@deborahwrites.com>
2025-07-24 10:23:22 +0100

    add text area; add custom script to send feedback;

--- docs/_extra/javascript/feedback.js
+++ docs/_extra/javascript/feedback.js
@@ -1,32 +1,40 @@
-// document$.subscribe(function() {
+document$.subscribe(function() {
     
-//   var feedback = document.forms.feedback
-//   console.log(feedback);
-//   console.log(typeof feedback);
-//   if (typeof feedback === "undefined") return
+  var feedback = document.forms.feedback
+  if (typeof feedback === "undefined") return
 
-//   feedback.hidden = false 
+  feedback.hidden = false 
 
-//   // Add this inside your subscribe function, after feedback is defined
-// feedback.querySelectorAll("button").forEach(btn => {
-//   btn.addEventListener("click", () => console.log("button clicked"));
-// });
+  // Add this inside your subscribe function, after feedback is defined
+feedback.querySelectorAll("button").forEach(btn => {
+  btn.addEventListener("click", () => console.log("button clicked"));
+});
 
-//   feedback.addEventListener("submit", function(ev) {
-//     alert("in event");
-//     ev.preventDefault()    
-//     var page = document.location.pathname 
-//     var data = ev.submitter.getAttribute("data-md-value")
+  feedback.addEventListener("submit", function(ev) {
+    ev.preventDefault()    
+    console.log(ev)
+    var page = document.location.pathname 
+    var rating = ev.submitter.getAttribute("data-md-value")
+    var feedbackComment = ev.target.querySelector("textarea#feedback-comment").value
 
-//     // replace with code to send to GA
-//     console.log(page, data) 
+    // replace with code to send to GA
+    console.log(page, rating, feedbackComment) 
+    // Send event to Google Tag Manager
+    window.dataLayer = window.dataLayer || [];
+    window.dataLayer.push({
+        event: "feedback",
+        page: page,
+        rating: rating,
+        feedbackComment: feedbackComment
+    });
 
-//     feedback.firstElementChild.disabled = true 
+    feedback.firstElementChild.disabled = true 
+    document.getElementById("feedback-comment").style.display = "none";
 
-//     var note = feedback.querySelector(
-//       ".md-feedback__note [data-md-value='" + data + "']"
-//     )
-//     if (note)
-//       note.hidden = false 
-//   })
-// })
+    var note = feedback.querySelector(
+      ".md-feedback__note [data-md-value='" + rating + "']"
+    )
+    if (note)
+      note.hidden = false 
+  })
+})
